1b4f851a78057f4529f99d389d36bc7b30becaed
himanshushukla31 <72408932+himanshushukla31@users.noreply.github.com>
2023-11-15 09:46:55 +0530

    UI modifications

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaBriefcase, FaCalendarAlt } from 'react-icons/fa';
+import { FaBriefcase} from 'react-icons/fa';
 import Typical from 'react-typical';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
